bbe9dca43278af541a0b251e1876aa13770845e2
Jasper Miller-Waugh <contact@jmw.nz>
2021-12-13 22:49:41 +1300

    Some styles for good measure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { useRef, useState } from "react";
 import {InvalidPDFException} from "pdfjs-dist/legacy/build/pdf.js";
 
+import styled from 'styled-components';
 
 import './App.css';
 
 import ScanCanvasPDF from "./components/ScanCanvasPDF";
 import FileUploader from "./components/FileUploader";
+
+const ScanButton = styled.button`
+	background-color: #4CAF50;
+	border: none;
+	color: white;
+	padding: 0.5rem 1rem;
+	text-align: center;
+	text-decoration: none;
+	font-size: 1rem;
+	cursor: pointer;
+	margin: 1em;
+	transition: all 0.3s ease-in-out;
+	&:disabled {
+		background-color: rgba(239, 239, 239, 0.3);
+		cursor: default;
+	}
+	&:active {
+        transform: translateY(2px);
+    }
+`
 
 
 function App() {
@@ -15,6 +36,7 @@ function App() {
 	const [selectedFile, setSelectedFile] = useState(null);
 
 	async function scanPDF() {
+		setResultText("");
 		try {
 			const qrCode = await pdfScannerRef.current.scanPDF(selectedFile);
 			// It returns null if no QR code is found
@@ -34,13 +56,15 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<ScanCanvasPDF ref={pdfScannerRef}/>
-				<FileUploader onFileSelectError={(err) => { console.log(err); }} onFileSelectSuccess={(file)=>{setSelectedFile(file)}} />
-				<button onClick={scanPDF} disabled={selectedFile===null}>
-					Scan PDF
-				</button>
-				<span style={{height: "40vh", width: "50vw", fontSize: "0.8rem", overflowWrap: "anywhere", overflow: "auto", border: "white solid 1px"}}>{resultText}</span>
+				React PDF QR Scanner
 			</header>
+			<ScanCanvasPDF ref={pdfScannerRef}/>
+				<FileUploader onFileSelectError={(err) => { console.log(err); }} onFileSelectSuccess={(file)=>{setSelectedFile(file)}} />
+				<ScanButton onClick={scanPDF} disabled={selectedFile===null}>
+					Scan PDF
+				</ScanButton>
+				<span style={{height: "40vh", width: "50vw", fontSize: "0.8rem", overflowWrap: "anywhere", overflow: "auto", border: "white solid 1px"}}>{resultText}</span>
+
 		</div>
 	);
 }
--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import styled from 'styled-components';
+
+const StyledFileUploader = styled.label`
+    display: block;
+    border: 1px solid #4CAF50;
+    display: inline-block;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    cursor: pointer;
+    color: #4CAF50;
+    transition: all 0.3s ease-in-out;
+    &:hover {
+        background-color: #4CAF50;
+        color: #fff;
+    }
+    &:active {
+        transform: translateY(2px);
+    }
+`;
+
+const StyledFileName = styled.span`
+    display: block;
+    font-size: 1rem;
+    margin-bottom: 1rem;
+`;
 
 /**
  * File Uploader Success callback.
@@ -22,6 +48,8 @@ import React from 'react';
   * @param {onFileSelectError} props.onFileSelectError The email of the user.
  */
 export default function FileUploader({ onFileSelectSuccess, onFileSelectError }) {
+    const [fileName, setFileName] = useState("");
+
     /**
      * 
      * @param {React.ChangeEvent<HTMLInputElement>} e
@@ -32,13 +60,19 @@ export default function FileUploader({ onFileSelectSuccess, onFileSelectError })
         if (file.type !== "application/pdf") {
             onFileSelectError({ error: "File must be a PDF" });
         } else {
+            setFileName(file.name);
             onFileSelectSuccess(file);
         }
     };
 
     return (
         <div className="file-uploader">
-            <input type="file" onChange={handleFileInput} accept=".pdf"/>
+            <StyledFileName>{fileName}</StyledFileName>
+
+            <StyledFileUploader>
+                <input style={{display: "none"}} type="file" onChange={handleFileInput} accept=".pdf"/>
+                Choose PDF
+            </StyledFileUploader>
         </div>
     );
 };
